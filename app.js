a5408a8a83ad6306022edb901825716b8b5c3af8
Maxime Horcholle <maxime.horcholle@gmail.com>
2017-02-06 23:22:07 +0100

    initial commit

--- app.js
+++ app.js
@@ -10,5 +10,22 @@ var board = new five.Board();
 
 board.on("ready", function() {
   var lcd = new five.LCD({ pins: [7, 8, 9, 10, 11, 12] });
-  lcd.print("Hello world");
+  lcd.print("App is starting");
+
+  while (true) {
+      this.wait(10000, function() {
+        lcd.clear().cursor(0, 0).print("weather");
+      });
+
+      var currentlyScrobbling = false;
+      if(currentlyScrobbling === true) {
+          this.wait(10000, function() {
+              lcd.clear().cursor(0, 0).print("lastfm (current track)");
+          });
+      }
+
+      this.wait(10000, function() {
+        lcd.clear().cursor(0, 0).print("lastfm (last track)");
+      });
+  }
 });
