35720f0085a478b5f36afd0c48d3a6eae7d9a0cc
Maxime Horcholle <maxime.horcholle@gmail.com>
2017-02-10 23:27:41 +0100

    update app.js

--- app.js
+++ app.js
@@ -1,31 +1,84 @@
 #!/usr/bin/env node
+
 'use strict';
 
 const winston = require('winston');
 var config = require('config');
 var five = require("johnny-five");
 
-var board = new five.Board();
-var board = new five.Board();
+var lcd;
+var loop = true;
+
+var sequence = [{
+    name: 'Weather',
+    method: function() {
+        lcd.clear().cursor(0, 0).print("weather");
+        board.info("LCD", 'weather');
+    },
+    enable: function() {
+        return true;
+    },
+    duration: 5000
+}, {
+    name: 'Lastfm (current track)',
+    method: function() {
+        lcd.clear().cursor(0, 0).print("lastfm (current track)");
+        board.info("LCD", 'lastfm (current track)');
+    },
+    enable: function() {
+        return true;
+    },
+    duration: 5000
+}, {
+    name: 'Lastfm (last track)',
+    method: function() {
+        lcd.clear().cursor(0, 0).print("Lastfm (last track)");
+        board.info("LCD", 'Lastfm (last track)');
+    },
+    enable: function() {
+        return true;
+    },
+    duration: 5000
+}];
+
+function execute(step) {
+
+    var method = sequence[step].method;
+    var enabled = sequence[step].enable();
+    var duration = sequence[step].duration || 3000;
+
+    step++;
+
+    if (enabled === true) {
+        method(lcd);
+    } else {
+        duration = 0;
+    }
+
+    if (step === sequence.length) {
+        if (loop) {
+            step = 0;
+        } else {
+            // We're done!
+            process.exit(0);
+        }
+    }
+
+    board.wait(duration, function() {
+        execute(step);
+    });
+}
+
+var board = new five.Board({
+    repl: false,
+});
 
 board.on("ready", function() {
-  var lcd = new five.LCD({ pins: [7, 8, 9, 10, 11, 12] });
-  lcd.print("App is starting");
+    lcd = new five.LCD({
+        pins: [7, 8, 9, 10, 11, 12]
+    });
+    lcd.print("App is starting");
+    board.info('APP', 'starting');
 
-  while (true) {
-      this.wait(10000, function() {
-        lcd.clear().cursor(0, 0).print("weather");
-      });
-
-      var currentlyScrobbling = false;
-      if(currentlyScrobbling === true) {
-          this.wait(10000, function() {
-              lcd.clear().cursor(0, 0).print("lastfm (current track)");
-          });
-      }
-
-      this.wait(10000, function() {
-        lcd.clear().cursor(0, 0).print("lastfm (last track)");
-      });
-  }
+    execute(0);
 });
